1c60e1afbf11cd9c316663d75837deb68ade2bbe
Trenton Bullard <trent.bullard@gmail.com>
2026-02-13 11:10:02 -0600

    add icons

---
+++ scripts/release/generate_icons.bat
@@ -0,0 +1,3 @@
+magick assets\logo\voidbreaker.png -define icon:auto-resize=256,128,64,48,32,16 assets\logo\voidbreaker.ico
+
+magick assets\logo\voidbreaker.png -define icon:auto-resize=1024,512,256,128,64,32,16 assets\logo\voidbreaker.icns
